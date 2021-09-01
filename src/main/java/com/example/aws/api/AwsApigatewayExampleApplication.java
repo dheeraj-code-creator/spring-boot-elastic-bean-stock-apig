6457ce4ab23193a916f2f16de61cf0957f142adb
Dheeraj <dheeraj1990kk@gmail.com>
2021-09-01 20:38:32 +0530

    added code

---
+++ src/main/java/com/example/aws/api/AwsApigatewayExampleApplication.java
@@ -0,0 +1,11 @@
+package com.example.aws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsApigatewayExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AwsApigatewayExampleApplication.class, args);
+    }
+}
